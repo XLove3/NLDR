401c01f7be40dfc592803615f1942b20a48663d2
XLove3 <xander.n.love@gmail.com>
2025-06-22 21:28:40 -0500

    Created comments to organize the future code, as well as initialized global variables and a 'show' function to switch between screens later.

--- main.py
+++ main.py
@@ -1,8 +1,50 @@
 # Import DearPyGui and the Random module:
 import dearpygui.dearpygui as dpg, random as rd
 
+# *** Global Variables: ***
+
+# NLDR will be set up into a dictionary of "screens":
+screens = {}
+# Set up the names for the dice:
+dice_names = ["Two", "Four", "Six", "Ten", "Twelve", "Twenty", "One_Hundred"]
+
+# Regular Mode states (checkboxes, sliders, and selected items):
+r_dice_boxes = {}
+r_dice_sliders = {}
+r_selected_items = {}
+
+# Average Mode states (checkboxes, sliders, and selected items):
+a_dice_boxes = {}
+a_dice_sliders = {}
+a_selected_items = {}
+
+# *** Starting Setup ***
+
+dpg.create_context()
+dpg.create_viewport(title='Neat Little Dice Roller')
+dpg.setup_dearpygui()
+
+
+# *** Functions: ***
+
+# Show screen according to name of screen:
+def show(name):
+    # TODO: Add a docstring to function show()
+    '''Docstring'''
+
+    # Loop through the values in the dictionary screens and hide each item:
+    for screen in screens.values():
+        dpg.hide_item(screen)
+
+    # Show the screen with the name from the argument name:
+    dpg.show_item(screens[name])
+
+
+# *** Temporary Code to Check Program Function ***
+
 # Callback function to print "user_data: "
 # and the user_data sent to the function:
+
 def callback(sender, app_data, user_data):
     # TODO: Add a docstring to function callback()
     '''Docstring'''
@@ -10,16 +52,6 @@ def callback(sender, app_data, user_data):
     print(f"user_data: {user_data}")
 
 
-# DPG - create context:
-dpg.create_context()
-# DPG - create viewport with the game title,
-# and a starting width, height. Viewport is resizable:
-dpg.create_viewport(title='Neat Little Dice Roller', width=1000, height=600)
-# DPG - set up DearPyGui:
-dpg.setup_dearpygui()
-
-
-# Create a new window with an appropriate label:
 with dpg.window(label="Main"):
     # Initialize some data to send to callback function:
     data = "From 'Main' window, button 'Push!'"
@@ -31,6 +63,26 @@ with dpg.window(label="Main"):
     # and callback the callback function:
     dpg.add_button(label="Push!", user_data=data, callback=callback)
 
+
+# *** Welcome Screen ***
+# TODO: Create a function to create the Welcome Screen
+
+
+# *** Main Menu ***
+# TODO: Create a function to create the Main Menu
+# (choice between Regular and Average modes)
+
+
+# *** Regular Mode ***
+# TODO: Create functions that create the Regular Mode gameplay
+
+
+# *** Average Mode ***
+# TODO: Create functions that create the Average Mode gameplay
+
+
+# *** Setup and Run ***
+# TODO: Make sure app runs
 
 # DPG - Show the Viewport:
 dpg.show_viewport()
